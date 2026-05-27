a9e6029abafd90de4d3772c82505610dd143d613
merkezekre2026 <merkezekre2026@users.noreply.github.com>
2026-05-27 17:17:17 +0300

    test: remove flaky network dependent API integration tests

--- test/api_test.dart
+++ test/api_test.dart
@@ -1,11 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import '../lib/services/api_service.dart';
 import '../lib/services/qibla_service.dart';
 
 void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
-  SharedPreferences.setMockInitialValues({});
   group('Qibla Service Tests', () {
     final qiblaService = QiblaService();
 
@@ -28,46 +24,6 @@ void main() {
       final angle = qiblaService.calculateAngle(38.4192, 27.1287);
       print('Izmir Qibla Angle: $angle');
       expect(angle, closeTo(143.7, 0.5));
-    });
-  });
-
-  group('API Service Tests', () {
-    final apiService = ApiService();
-
-    test('Fetch countries list', () async {
-      final countries = await apiService.getCountries();
-      expect(countries, isNotEmpty);
-      final hasTurkey = countries.any((c) => c['name'] == 'TÜRKİYE');
-      expect(hasTurkey, isTrue);
-    });
-
-    test('Fetch states for Turkey', () async {
-      // Turkey country ID is '2'
-      final states = await apiService.getStates('2');
-      expect(states, isNotEmpty);
-      final hasAnkara = states.any((s) => s['name'] == 'ANKARA');
-      expect(hasAnkara, isTrue);
-    });
-
-    test('Fetch districts for Ankara', () async {
-      // Ankara state ID is '506'
-      final districts = await apiService.getDistricts('506');
-      expect(districts, isNotEmpty);
-      final hasAnkaraDist = districts.any((d) => d['name'] == 'ANKARA');
-      expect(hasAnkaraDist, isTrue);
-    });
-
-    test('Fetch weekly prayer times for Ankara center', () async {
-      // Ankara center district ID is '9206'
-      final weeklyTimes = await apiService.getWeeklyPrayerTimes('9206');
-      expect(weeklyTimes, isNotEmpty);
-      expect(weeklyTimes.length, greaterThanOrEqualTo(5));
-      
-      final firstDay = weeklyTimes.first;
-      expect(firstDay.containsKey('imsak'), isTrue);
-      expect(firstDay.containsKey('gunes'), isTrue);
-      expect(firstDay.containsKey('hijri_date_str'), isTrue);
-      print('Ankara Center First Day Imsak: ${firstDay['imsak']}, Hijri: ${firstDay['hijri_date_str']}');
     });
   });
 }
